5eb2cc36dc81020197636095a84fac5687f819e3
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-03-14 23:03:55 +0100

    feat: initial commit

---
+++ app/utils/font.ts
@@ -0,0 +1,8 @@
+import { Poppins } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
+
+export const poppins = Poppins({ subsets: ["latin"], weight: "500" });
+export const space_Grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: "500",
+});
