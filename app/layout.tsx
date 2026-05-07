48ee9bc62b8b2c134215e235c9099bd65d50716e
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-05-07 10:46:51 +0100

    redeployment changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { AuthProvider } from "./Authprovider";
 import "./globals.css";
 
 import Navbar from "./_components/Navbar";
@@ -38,10 +39,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
-        <Footer />
-        <ToastProvider />
+        <AuthProvider>
+          <Navbar />
+          {children}
+          <Footer />
+          <ToastProvider />
+        </AuthProvider>
       </body>
     </html>
   );
