90531e19671b1244fbcb19dd2adc64353df37554
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-05-07 11:20:16 +0100

    fixing kinde error

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "via.placeholder.com",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -39,6 +13,16 @@ const nextConfig: NextConfig = {
         hostname: "via.placeholder.com",
       },
     ],
+  },
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/dashboard`,
   },
 };
 
