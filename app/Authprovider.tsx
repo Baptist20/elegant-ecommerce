9c3ef5c5c7c98fbb0c687c0086403e822eec04a2
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-05-07 09:57:39 +0100

    fixed children bug in KindeAuthProvider

--- app/Authprovider.tsx
+++ app/Authprovider.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return <KindeProvider>{children}</KindeProvider>;
 };
