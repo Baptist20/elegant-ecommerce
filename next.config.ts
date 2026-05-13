aaafde8bd9a063bbb85e5018fe0209393c445334
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-05-13 09:31:24 +0100

    removed dynamic env from next config

--- next.config.ts
+++ next.config.ts
@@ -14,18 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    // If VERCEL_URL exists (on Vercel), use it. Otherwise, use your local/prod env var.
-    KINDE_SITE_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : process.env.KINDE_SITE_URL,
-    KINDE_POST_LOGIN_REDIRECT_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/dashboard`
-      : process.env.KINDE_POST_LOGIN_REDIRECT_URL,
-    KINDE_POST_LOGOUT_REDIRECT_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
-  },
 };
 
 export default nextConfig;
